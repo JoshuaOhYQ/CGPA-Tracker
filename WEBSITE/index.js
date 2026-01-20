a68fc06109e842335bdc976140e4f411eef37753
JoshuaOhYQ <yuqiao.oh@gmail.com>
2026-01-20 22:09:51 +0800

    Update index.js

--- WEBSITE/index.js
+++ WEBSITE/index.js
@@ -7,12 +7,43 @@ console.log(`I like Pizza!`);
 document.getElementById("myH1").textContent = `Hello`;
 document.getElementById("myP").textContent = `I like Pizza!`;
 
+let age = 25;
+let price = 10.99;
+let gpa = 3.7;
+/*
+console.log(age);
+console.log(price);
+console.log(gpa);
+
+
+console.log(`You are ${age} years old.`);
+console.log(`The price is $${price}.`);
+console.log(`Your GPA is ${gpa}.`);
+*/
+
+/*
+let firstName = "John";
+let favouriteFood = "Pizza";
+let email = "John123@gmail.com";
+console.log(typeof firstName); 
+console.log(`Your name is ${firstName}.`);
+console.log(`You like ${favouriteFood}.`);
+console.log(`Your email is ${email}.`); 
+*/
+
+let online = true;
+console.log(typeof online);
+
+
 
 
 // This is a comment 
 
 /* 
 This is a comment
+
+
+
 
 
 
